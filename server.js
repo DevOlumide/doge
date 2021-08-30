1d9156f393fff62d0d9500fa22e71728c8721500
Samuel <olumidesamuel1914@gmail.com>
2021-08-30 18:23:25 +0100

    m

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ app.use("/app", routes);
 
 
 if(process.env.NODE_ENV === "production"){
- app.use(express.static(path.join(__dirname, 'client/build')));
- 
- app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
- }
+ app.use(express.static('client/build'));
 }
 
 
